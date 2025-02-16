5f086f2b1e7ed51cd790a7c66531ef3d67fcfc86
João Pedro Gonçalves <jpedro.fsgs@gmail.com>
2025-02-16 13:17:40 -0300

    feat: layout changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
-import TreeView from "@/components/TreeView";
+import Link from "next/link";
 
 export default function Home() {
-
-
     return (
-        <div className="w-dvw h-dvh bg-background">
-          <TreeView />
+        <div className="min-h-screen flex">
+            <div className="flex-1 flex items-center justify-center transition-colors duration-300 text-primary hover:bg-secondary">
+                <Link
+                    href="/radix"
+                    className="text-6xl hover:underline"
+                >
+                    Radix
+                </Link>
+            </div>
+            <div className="flex-1 flex items-center justify-center transition-colors duration-300 text-primary hover:bg-secondary">
+                <Link
+                    href="/trie"
+                    className="text-6xl hover:underline"
+                >
+                    Trie
+                </Link>
+            </div>
         </div>
     );
 }

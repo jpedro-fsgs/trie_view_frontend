1607ca2ef66f52770fe3dccf1ce0f032bdd684b5
João Pedro Gonçalves <jpedro.fsgs@gmail.com>
2025-02-16 13:16:48 -0300

    feat: radix tree

---
+++ src/app/radix/page.tsx
@@ -0,0 +1,12 @@
+import TreeView from "@/components/TreeView";
+import React from "react";
+
+function RadixPage() {
+    return (
+        <div className="w-dvw h-dvh bg-background">
+            <TreeView treeType="radix" />
+        </div>
+    );
+}
+
+export default RadixPage;
